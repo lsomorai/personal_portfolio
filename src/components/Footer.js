3f904a61620142326eb48ecdd5ae30ecb25a846d
Lucien Somorai <lsomorai3@gmail.com>
2025-01-08 10:08:44 -0700

    Completed the navbar, footer and home page content

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Instagram from '@mui/icons-material/Instagram';
+import Twitter from '@mui/icons-material/Twitter';
+import Facebook from '@mui/icons-material/Facebook';
+import LinkedIn from '@mui/icons-material/LinkedIn';
+import "../styles/Footer.css";
+
+function Footer() {
+    return (
+    <div className="footer">
+        <div className="socialMedia">
+            <Instagram />
+            <Twitter />
+            <Facebook />
+            <LinkedIn />
+        </div>
+        <p> &copy; 2025 luciensomorai.com</p>
+    </div>
+    );
+}
+
+export default Footer;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,47 @@
 import React from 'react'
+import LinkedIn from '@mui/icons-material/LinkedIn';
+import Email from '@mui/icons-material/Email';
+import Github from '@mui/icons-material/GitHub';
+import "../styles/Home.css";
 
 function Home(){
     return (
-        <div>Home</div>
-    )
+        <div className="home">
+            <div className="about">
+                <h2>Hi, My Name is Lucien</h2>
+                <div className="prompt">
+                    <p>
+                        A software developer with a passion for learning and creating.
+                    </p>
+                    <LinkedIn />
+                    <Email />
+                    <Github />
+                </div>
+            </div>
+            <div className="skills"></div>
+                <h1> Skills</h1>
+                <ol className="list">
+                    <li className="item">
+                        <h2> Front-End</h2>
+                        <span>
+                            ReactJS, HTML, CSS
+                        </span>
+                    </li>
+                    <li className="item">
+                        <h2> Back-End</h2>
+                        <span> 
+                            NodeJS, Springboot, Django, MySQL    
+                        </span>
+                    </li>
+                    <li className="item">
+                        <h2> Languages</h2>
+                        <span> 
+                            JavaScript, Java, Python, C, C++
+                        </span>
+                    </li>
+                </ol>
+        </div>
+    );
 }
 
 export default Home
