eb4e560f6b255871689fd32a99368727905d4b9b
Lucien Somorai <lsomorai3@gmail.com>
2025-01-20 14:01:52 -0700

    fixed issue with the background

--- src/components/CursorAnimation.js
+++ src/components/CursorAnimation.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import '../styles/CursorAnimation.css';
 
 const CursorAnimation = () => {
-  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+  const [cursorPosition, setCursorPosition] = useState(null);
 
   const handleMouseMove = (e) => {
     setCursorPosition({ x: e.clientX, y: e.clientY });
@@ -10,13 +10,15 @@ const CursorAnimation = () => {
 
   return (
     <div className="cursor-animation" onMouseMove={handleMouseMove}>
-      <div
-        className="cursor-hole"
-        style={{
-          left: `${cursorPosition.x}px`,
-          top: `${cursorPosition.y}px`,
-        }}
-      ></div>
+      {cursorPosition && (
+        <div
+          className="cursor-hole"
+          style={{
+            left: `${cursorPosition.x}px`,
+            top: `${cursorPosition.y}px`,
+          }}
+        ></div>
+      )}
     </div>
   );
 };
