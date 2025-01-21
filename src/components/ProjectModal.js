5c719f482087fee137694f4ee6706b0acfeea05a
Lucien Somorai <lsomorai3@gmail.com>
2025-01-20 19:41:30 -0700

    added github icon to project modals

--- src/components/ProjectModal.js
+++ src/components/ProjectModal.js
@@ -1,18 +1,18 @@
 import React from 'react';
+import { FaGithub } from 'react-icons/fa'; 
 import '../styles/ProjectModal.css';
 
 const ProjectModal = ({ project, onClose }) => {
   return (
     <div className="project-modal" onClick={onClose}>
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-        {/* Prevent click events from bubbling up */}
         <button className="close-button" onClick={onClose}>
           &times;
         </button>
         <h2>{project.title}</h2>
         <p>{project.description}</p>
-        <a href={project.githubLink} target="_blank" rel="noopener noreferrer">
-          View on GitHub
+        <a href={project.githubLink} target="_blank" rel="noopener noreferrer" className="github-link"> 
+          <FaGithub className="icon" />
         </a>
       </div>
     </div>
