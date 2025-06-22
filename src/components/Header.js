0f0ce35fe126e51aa8b45ec36098e57eb57eaec6
Lucien Somorai <lsomorai3@gmail.com>
2025-06-21 22:14:19 -0700

    fixed sun and moon icon overlap

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,18 @@ import { FaSun, FaMoon } from 'react-icons/fa';
 const Header = ({ darkMode, onToggleTheme }) => (
   <header className="header">
      <nav className="navbar">
-      <button onClick={onToggleTheme} className="theme-toggle" aria-label="Toggle theme">
-        {darkMode ? <FaSun /> : <FaMoon />}
-      </button>
-      <ul>
+      <ul className="nav-links">
         <li><a href="#home">Home</a></li>
         <li><a href="#projects">Projects</a></li>
         <li><a href="#experience">Experience</a></li>
       </ul>
+      <button
+        onClick={onToggleTheme}
+        className="theme-toggle"
+        aria-label="Toggle theme"
+      >
+        {darkMode ? <FaSun /> : <FaMoon />}
+      </button>
     </nav>
   </header>
 );
